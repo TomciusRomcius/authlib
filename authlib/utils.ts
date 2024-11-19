06261742ac4c03c279223de8abd74c66bf08a7d9
Tomas <tomas.ew88@gmail.com>
2024-11-19 15:11:14 +0200

    Ignore linter for any type on safeJsonParse

--- authlib/utils.ts
+++ authlib/utils.ts
@@ -2,9 +2,12 @@
 __proto__ is disabled in Deno, but just in case, 
 will add this safer JSON parse function :)
 */
+
+// deno-lint-ignore no-explicit-any
 const reviver = (key: string, value: any) =>
   key === "__proto__" ? undefined : value;
 
-export function safeJsonParse(jsonText: string) {
+// deno-lint-ignore no-explicit-any
+export function safeJsonParse(jsonText: string): any {
   return JSON.parse(jsonText, reviver);
 }
